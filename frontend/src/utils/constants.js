052d94be1453457e8457199d636a988e0d232920
Med amine Tajaoui <Mohamed Amine>
2024-12-10 13:58:36 +0100

    .

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,9 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "../../../.env" });
 
-export const API_URL =
+/* export const API_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:8000/api"
     : "https://mern-e-commerce-mrt2.onrender.com/api";
 
-console.log(API_URL);
+console.log(API_URL); */
+
+export const API_URL = "https://mern-e-commerce-mrt2.onrender.com/api";

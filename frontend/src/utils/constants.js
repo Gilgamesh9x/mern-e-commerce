8e44fd0ad7d0d43c8bd1117f1434b93503c41a02
Med amine Tajaoui <Mohamed Amine>
2024-12-10 14:04:10 +0100

    .

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
-import dotenv from "dotenv";
-dotenv.config({ path: "../../../.env" });
+/* import dotenv from "dotenv";
+dotenv.config({ path: "../../../.env" }); */
 
 /* export const API_URL =
   process.env.NODE_ENV === "development"
